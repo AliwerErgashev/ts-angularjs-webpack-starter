7a4618671aecff28b8fd1beee6b15969594676ec
ravshan <ravshansbox@gmail.com>
2017-12-13 14:42:47 +0500

    body-parser setup;

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
 import { api } from "./api";
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
 app.use("/api", api);
 
-app.listen(9000, () => console.log("HTTP server up on port 9000"));
+app.listen(9000, () => console.info("HTTP server up on port 9000"));
